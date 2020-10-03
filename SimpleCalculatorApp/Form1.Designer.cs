2faea97ddc07d9e9f922a7cfd0328fb19f871f77
Airolle de Vera <airolledevera3@gmail.com>
2020-10-03 11:25:20 +0800

    Simple Calculator

---
+++ SimpleCalculatorApp/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace SimpleCalculatorApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.Number1 = new System.Windows.Forms.TextBox();
+            this.Number2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Arial Black", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(43, 166);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(120, 40);
+            this.button5.TabIndex = 0;
+            this.button5.Text = "Addtion";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Arial Black", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(203, 166);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(120, 40);
+            this.button6.TabIndex = 1;
+            this.button6.Text = "Substraction";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Arial Black", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(203, 243);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(120, 40);
+            this.button7.TabIndex = 2;
+            this.button7.Text = "Division";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Arial Black", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(43, 243);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(120, 40);
+            this.button8.TabIndex = 3;
+            this.button8.Text = "Multiplication";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // Number1
+            // 
+            this.Number1.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Number1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.Number1.Location = new System.Drawing.Point(133, 46);
+            this.Number1.Name = "Number1";
+            this.Number1.Size = new System.Drawing.Size(200, 20);
+            this.Number1.TabIndex = 4;
+            this.Number1.TextChanged += new System.EventHandler(this.Number1_TextChanged);
+            // 
+            // Number2
+            // 
+            this.Number2.BackColor = System.Drawing.SystemColors.Window;
+            this.Number2.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Number2.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.Number2.Location = new System.Drawing.Point(133, 98);
+            this.Number2.Name = "Number2";
+            this.Number2.Size = new System.Drawing.Size(200, 20);
+            this.Number2.TabIndex = 5;
+            this.Number2.TextChanged += new System.EventHandler(this.Number2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(22, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Input Number :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(22, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Input Number :";
+            // 
+            // Form1
+            // 
+            this.AccessibleDescription = "";
+            this.AccessibleName = "";
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(367, 320);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Number2);
+            this.Controls.Add(this.Number1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Font = new System.Drawing.Font("Arial Black", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Simple Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.TextBox Number1;
+        private System.Windows.Forms.TextBox Number2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ SimpleCalculatorApp/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleCalculatorApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            GetAnswer delegateAnswer = new GetAnswer(Formula.getSum);
+
+            int num1 = Int32.Parse(Number1.Text);
+            int num2 = Int32.Parse(Number2.Text);
+            string title = "Answer: ";
+
+            MessageBox.Show(delegateAnswer(num1, num2).ToString(), title);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            GetAnswer delegateAnswer = new GetAnswer(Formula.getDifference);
+
+            int num1 = Int32.Parse(Number1.Text);
+            int num2 = Int32.Parse(Number2.Text);
+            string title = "Answer: ";
+
+            MessageBox.Show(delegateAnswer(num1, num2).ToString(), title);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            GetAnswer delegateAnswer = new GetAnswer(Formula.getProduct);
+
+            int num1 = Int32.Parse(Number1.Text);
+            int num2 = Int32.Parse(Number2.Text);
+            string title = "Answer: ";
+
+            MessageBox.Show(delegateAnswer(num1, num2).ToString(), title);
+
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+            GetAnswer delegateAnswer = new GetAnswer(Formula.getQuotient);
+
+            int num1 = Int32.Parse(Number1.Text);
+            int num2 = Int32.Parse(Number2.Text);
+            string title = "Answer: ";
+
+            MessageBox.Show(delegateAnswer(num1, num2).ToString(), title);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Number1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void Number2_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
